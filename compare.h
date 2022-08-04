51c658f682c8790e482866c7bd9f8120258ea9db
yonghun <kimmo1028@gmail.com>
2022-08-04 17:29:14 +0900

    함수 오버로딩의 예시

---
+++ compare.h
@@ -0,0 +1,11 @@
+#ifndef __COMPARE_H__
+#define __COMPARE_H__
+
+#include <iostream>
+
+int Compare(const int a, const int b);
+int Compare(const float a, const float b);
+int Compare(const char a, const char b);
+int Compare(const char str1p[], const char str2[]);
+
+#endif //__COMPARE_H__
